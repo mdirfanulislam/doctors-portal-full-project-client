4f3ea8b72eecd8bd490541533e88994cd94d97bd
Mohammad Irfanul Islam <irfanul009@gmail.com>
2021-12-28 07:11:33 +0600

    first commit

---
+++ src/Hooks/UseFirebase.js
@@ -0,0 +1,128 @@
+import { useState, useEffect } from 'react';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, GoogleAuthProvider, signInWithPopup, updateProfile,getIdToken, signOut } from "firebase/auth";
+import initializeAuthentication from './../Pages/Login/Login/Firebase/FirebaseInit';
+
+// initialize firebase app
+initializeAuthentication();
+
+const useFirebase = () => {
+    const [user, setUser] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
+    const [authError, setAuthError] = useState('');
+    const [admin, setAdmin] = useState(false);
+    const [token, setToken] = useState('');
+
+    const auth = getAuth();
+    const googleProvider = new GoogleAuthProvider();
+
+    const registerUser = (email, password, name, history) => {
+        setIsLoading(true);
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(() => {
+                setAuthError('');
+                const newUser = { email, displayName: name };
+                setUser(newUser);
+                // save user to the database
+                saveUser(email, name, 'POST');
+                // send name to firebase after creation
+                updateProfile(auth.currentUser, {
+                    displayName: name
+                }).then(() => {
+                }).catch(() => {
+                });
+                history.replace('/');
+            })
+            .catch((error) => {
+                setAuthError(error.message);
+                console.log(error);
+            })
+            .finally(() => setIsLoading(false));
+    }
+
+    const loginUser = (email, password, location, history) => {
+        setIsLoading(true);
+        signInWithEmailAndPassword(auth, email, password)
+            .then(() => {
+                const destination = location?.state?.from || '/';
+                history.replace(destination);
+                setAuthError('');
+            })
+            .catch((error) => {
+                setAuthError(error.message);
+            })
+            .finally(() => setIsLoading(false));
+    }
+
+    const signInWithGoogle = (history, location) => {
+        setIsLoading(true);
+        signInWithPopup(auth, googleProvider)
+            .then(() => {
+                saveUser(user.email, user.displayName, 'PUT');
+                setAuthError('');
+                const destination = location?.state?.from || '/';
+                history.replace(destination);
+            }).catch((error) => {
+                setAuthError(error.message);
+            }).finally(() => setIsLoading(false));
+    }
+
+    // observer user state
+    useEffect(() => {
+        const unsubscribed = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setUser(user);
+                getIdToken(user)
+                .then(idToken => {
+                    setToken(idToken)
+                })
+            } else {
+                setUser({})
+            }
+            setIsLoading(false);
+        });
+        return () => unsubscribed;
+    }, []);
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/users/${user.email}`)
+        .then(res => res.json())
+        .then(data => setAdmin(data.admin));
+    }, [user.email])
+
+    const logout = () => {
+        setIsLoading(true);
+        signOut(auth).then(() => {
+            // Sign-out successful.
+        }).catch((error) => {
+            // An error happened.
+        })
+            .finally(() => setIsLoading(false));
+    }
+
+    const saveUser = ( email, displayName, method ) => {
+        const user = { email, displayName };
+        fetch('http://localhost:5000/users', {
+            method: method,
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+        .then(res => res.json())
+        .then(data => console.log(data))
+    }
+
+    return {
+        user,
+        admin,
+        token,
+        isLoading,
+        authError,
+        registerUser,
+        loginUser,
+        signInWithGoogle,
+        logout,
+    }
+}
+
+export default useFirebase;
---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import { Container } from '@mui/material';
+import fluoride from '../../../images/fluoride.png';
+import cavity from '../../../images/cavity.png';
+import whitening from '../../../images/whitening.png';
+import Service from '../Service/Service';
+import Typography from '@mui/material/Typography';
+
+const services = [
+    {
+        id: 1, name: "Flouride Treatment", description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, dolor molestiae dicta quos esse voluptatibus! Maxime accusamus cum unde mollitia?", img: fluoride
+    },
+    
+    {
+        id: 2, name: "Cavity Filling", description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, dolor molestiae dicta quos esse voluptatibus! Maxime accusamus cum unde mollitia?", img: cavity
+    },
+    
+    {
+        id: 3, name: "Teeth Whitening", description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, dolor molestiae dicta quos esse voluptatibus! Maxime accusamus cum unde mollitia?", img: whitening
+    }
+  ]
+
+const Services = () => {
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <Container>
+                <Typography sx={{ fontWeight: 500, m: 2, color: 'success.main' }} variant="h6" component="div">
+                    OUR SERVICES
+                    </Typography>
+                <Typography sx={{ fontWeight: 600, m: 5 }} variant="h4" component="div">
+                    Services We Provide
+                    </Typography>
+                <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+                    {
+                        services.map(service => <Service 
+                        service={service}
+                        ></Service>)
+                    }
+                </Grid>
+            </Container>
+        </Box>
+    );
+};
+
+export default Services;
